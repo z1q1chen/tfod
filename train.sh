c0066d7bf286cc96d510c379e3b08ce06226bb93
Ziqi <jackychencw@gmail.com>
2021-07-19 02:08:19 -0400

    update

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-CHOSEN_MODEL='SSD-ResNet152-V1-FPN-1024'
+#!/bin/sh
 PIPELINE_FILE='research/deploy/pipeline_file.config'
 NUM_STEPS=40000
 NUM_EVAL_STEPS=500
@@ -12,4 +12,4 @@ python research/object_detection/model_main_tf2.py \
     --num_eval_steps=$NUM_EVAL_STEPS
 
 
-python research/object_detection/model_main_tf2.py --pipeline_config_path=./research/deploy/pipeline_file.config --model_dir=training --alsologtostderr --num_train_steps=40000 --sample_1_of_n_eval_examples=1 --num_eval_steps=500
+# python research/object_detection/model_main_tf2.py --pipeline_config_path=./research/deploy/pipeline_file.config --model_dir=training --alsologtostderr --num_train_steps=40000 --sample_1_of_n_eval_examples=1 --num_eval_steps=500
