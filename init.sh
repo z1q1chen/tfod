a9096407928928de3954130826a58ca263e0a21f
Ziqi <jackychencw@gmail.com>
2021-07-19 01:28:14 -0400

    updates

---
+++ init.sh
@@ -0,0 +1,12 @@
+# conda install protobuf -y
+# cd research
+# protoc object_detection/protos/*.proto --python_out=.
+# cp object_detection/packages/tf2/setup.py .
+# python -m pip install .
+# cd ..
+# pip install imageio
+# pip install wget
+# mkdir data
+# mkdir research/deploy
+# cd data
+# curl -L "https://app.roboflow.com/ds/no8u8m3Ifw?key=itfwqk4p5f" > roboflow.zip; unzip roboflow.zip; rm roboflow.zip
