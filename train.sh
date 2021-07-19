b8e12ee3db689dd0151dd06a18f93b6351b4917e
Ziqi <jackychencw@gmail.com>
2021-07-19 10:00:51 -0400

    update

--- train.sh
+++ train.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-PIPELINE_FILE='research/deploy/ssd_resnet152_v1_fpn_1024x1024_coco17_tpu-8/pipeline_file.config'
-NUM_STEPS=40000
-NUM_EVAL_STEPS=500
-MODEL_DIR='training'
-python research/object_detection/model_main_tf2.py \
-    --pipeline_config_path=$pipeline_file \
-    --model_dir=$MODEL_DIR \
-    --alsologtostderr \
-    --num_train_steps=$NUM_STEPS \
-    --sample_1_of_n_eval_examples=1 \
-    --num_eval_steps=$NUM_EVAL_STEPS
+# PIPELINE_FILE='research/deploy/ssd_resnet152_v1_fpn_1024x1024_coco17_tpu-8/pipeline_file.config'
+# NUM_STEPS=40000
+# NUM_EVAL_STEPS=500
+# MODEL_DIR='training'
+# python research/object_detection/model_main_tf2.py \
+#     --pipeline_config_path=$pipeline_file \
+#     --model_dir=$MODEL_DIR \
+#     --alsologtostderr \
+#     --num_train_steps=$NUM_STEPS \
+#     --sample_1_of_n_eval_examples=1 \
+#     --num_eval_steps=$NUM_EVAL_STEPS
 
 
-# python research/object_detection/model_main_tf2.py --pipeline_config_path=./research/deploy/ssd_resnet152_v1_fpn_1024x1024_coco17_tpu-8/pipeline_file.config --model_dir=training --alsologtostderr --num_train_steps=40000 --sample_1_of_n_eval_examples=1 --num_eval_steps=500
+python research/object_detection/model_main_tf2.py --pipeline_config_path=pipeline_file.config --model_dir=training --alsologtostderr --num_train_steps=40000 --sample_1_of_n_eval_examples=1 --num_eval_steps=500
+python research/object_detection/model_main_tf2.py --pipeline_config_path=pipeline_file.config --model_dir=training --checkpoint_dir=training 
