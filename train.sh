746991367403961ec4998c2cb461d6d1c8471a8e
Ziqi <jackychencw@gmail.com>
2021-07-23 03:13:33 -0400

    update

--- train.sh
+++ train.sh
@@ -14,3 +14,6 @@
 
 python research/object_detection/model_main_tf2.py --pipeline_config_path=pipeline_file.config --model_dir=training --alsologtostderr --num_train_steps=40000 --sample_1_of_n_eval_examples=1 --num_eval_steps=500
 python research/object_detection/model_main_tf2.py --pipeline_config_path=pipeline_file.config --model_dir=training --checkpoint_dir=training 
+
+MODEL_NAME='ssd_mobilenet_v2_320x320_coco17_tpu-8'
+python research/object_detection/model_main_tf2.py --pipeline_config_path=research/deploy/$MODEL_NAME/pipeline_file.config --model_dir=training_$MODEL_NAME --alsologtostderr --num_train_steps=40000 --sample_1_of_n_eval_examples=1 --num_eval_steps=500
