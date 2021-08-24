9d6f4ad80dbe5ce35da960a7f27b756fe6085187
Ziqi <jackychencw@gmail.com>
2021-08-24 05:05:59 -0400

    update

--- eval.sh
+++ eval.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-Models="SSD-MobileNet-V2-320 SSD-MobileNet-V1-FPN-640 SSD-MobileNet-V2-FPNlite-320 SSD-MobileNet-V2-FPNlite-640 SSD-ResNet50-V1-FPN-640 SSD-ResNet50-V1-FPN-1024 SSD-ResNet101-V1-FPN-640 SSD-ResNet101-V1-FPN-1024 SSD-ResNet152-V1-FPN-640 SSD-ResNet152-V1-FPN-1024"
+# Define a string variable with a value
+models="SSD-MobileNet-V2-320 \
+        SSD-MobileNet-V1-FPN-640 \
+        SSD-MobileNet-V2-FPNlite-320 \
+        SSD-MobileNet-V2-FPNlite-640 \
+        SSD-ResNet50-V1-FPN-640 \
+        SSD-ResNet50-V1-FPN-1024 \
+        SSD-ResNet101-V1-FPN-640 \
+        SSD-ResNet101-V1-FPN-1024 \
+        SSD-ResNet152-V1-FPN-640 \
+        SSD-ResNet152-V1-FPN-1024"
 
 # Iterate the string variable using for loop
-for model in $Models; do
-    echo Evaluating $model
-    python research/deeplab/eval.py --pipeline_config_path="${model}_pipeline_file.config" --checkpoint_dir="trained_models/training_${model}" --eval_dir="eval/eval_${model}" --logtostderr    
+for model in $models; do
+    echo Training $model
+    python research/object_detection/model_main_tf2.py \
+        --pipeline_config_path="models/${model}/pipeline.config" \
+        --model_dir="models/${model}/model" \
+        --checkpoint_dir="models/${model}/checkpoint" \
+        --alsologtostderr
 done
